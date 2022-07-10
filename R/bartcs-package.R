5a9672ae404d48ae2fd6fa53fe899374f8715fb1
Yeonghoon Yoo <yooyh.stat@gmail.com>
2022-07-10 20:54:54 +0900

    Update bartcs-package manual

--- R/bartcs-package.R
+++ R/bartcs-package.R
@@ -5,14 +5,26 @@
 ## usethis namespace: end
 NULL
 
-#' bartcs: Bayesian Nonparametric Adjustment of Confounding
+#' bartcs: Bayesian Additive Regression Trees for Confounder Selection
+#' 
+#' Fit Bayesian Regression Additive Trees (BART) models to
+#' select relevant confounders among a large set of potential confounders
+#' and to estimate average treatment effect. For more information, see
+#' Kim et al. (2022).
 #'
 #' Functions in `bartcs` serve one of three purposes.
 #' \enumerate{
-#'   \item Functions for fitting: `sbart()`, `mbart()`.
-#'   \item Functions for summary: `summary()`, `gelman_rubin()`, `plot()`
-#'   \item Util function for openMP: `count_omp_thread()`
+#'   \item Functions for fitting: `sbart()` and `mbart()`.
+#'   \item Functions for summary: `summary()`, `plot()` and `gelman_rubin()`.
+#'   \item Utility function for openMP: `count_omp_thread()`.
 #' }
+#' 
+#' @references
+#' Kim, C., Tec, M., & Zigler, C. M. (2022).
+#' Bayesian Nonparametric Adjustment of Confounding.
+#' *arXiv preprint arXiv:2203.11798*.
+#' \doi{10.48550/arXiv.2203.11798}
+#' 
 #' @docType package
 #' @name bartcs-package
 NULL
